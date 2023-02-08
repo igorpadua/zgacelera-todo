221cc5a6e10ae299327df7927cec11876e11cffc
igorpadua <igorpadua@tutanota.com>
2023-02-08 19:58:16 -0300

    Agora é possível filtar

--- Frontend/js/listTask.js
+++ Frontend/js/listTask.js
@@ -2,11 +2,39 @@ let taskList = JSON.parse(window.localStorage.getItem("taskList")) || [];
 
 // Lista todas as tarefas
 function listTasks() {
+	if (taskList.length == 0) {
+		alert("Nenhuma tarefa cadastrada!");
+		return ""
+	}
+	let lista = "<tr><th>Id</th><th>Nome</th><th>Descrição</th><th>Data</th><th>Prioridade</th><th>Categoria</th><th>Status</th></tr>"
+	for (var i = 0; i < taskList.length; i++) {
+		lista += "<tr>"
+		lista +=    "<td>" + i + "</td>"
+		lista += 		"<td>" + taskList[i].name + "</td>"
+		lista += 		"<td>" + taskList[i].description + "</td>"
+		lista += 		"<td>" + taskList[i].date + "</td>"
+		lista += 		"<td>" + taskList[i].priority + "</td>"
+		lista += 		"<td>" + taskList[i].categoria + "</td>"
+		lista += 		"<td>" + taskList[i].status + "</td>"
+		lista += "</tr>"
+	}
+
+		return lista;
+}
+
+document.getElementById("listarTask").innerHTML = listTasks();
+
+// Ordernar
+function odernarStatus(status) {
+
 	let lista = ""
 	if (taskList.length == 0) {
 		alert("Nenhuma tarefa cadastrada!");
-	} else {
-		for (var i = 0; i < taskList.length; i++) {
+		return
+	}
+	lista = "<tr><th>Id</th><th>Nome</th><th>Descrição</th><th>Data</th><th>Prioridade</th><th>Categoria</th><th>Status</th></tr>"
+	for (var i = 0; i < taskList.length; i++) {
+		if (taskList[i].status == status) {
 			lista += "<tr>"
 			lista +=    "<td>" + i + "</td>"
 			lista += 		"<td>" + taskList[i].name + "</td>"
@@ -17,11 +45,25 @@ function listTasks() {
 			lista += 		"<td>" + taskList[i].status + "</td>"
 			lista += "</tr>"
 		}
-
-		return lista;
 	}
-	return ""
+	document.getElementById("listarTask").innerHTML = lista;
 }
 
-document.getElementById("listarTask").innerHTML += listTasks();
+document.getElementById("filter-todo").onclick = function() {
+	odernarStatus("A fazer");
+}
 
+// Ordernar por doing
+document.getElementById("filter-doing").onclick = function() {
+	odernarStatus("Em andamento");
+}
+
+// Ordernar por done
+document.getElementById("filter-done").onclick = function() {
+	odernarStatus("Concluido");
+}
+
+// Ordernar por todas
+document.getElementById("filter-all").onclick = function() {
+	document.getElementById("listarTask").innerHTML = listTasks();
+}
