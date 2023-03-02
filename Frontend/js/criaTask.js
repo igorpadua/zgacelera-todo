b330eb1b7f377d9e16e654fa8c28e9a168b59c94
igorpadua <igorpadua@tutanota.com>
2023-03-02 15:01:37 -0300

    refactor: Criar task e listar tarefas

---
+++ Frontend/js/criaTask.js
@@ -0,0 +1,18 @@
+import {Task} from "./task";
+
+export function criaTask() {
+
+    const taskName = document.getElementById("taskName").value;
+    const taskDescription = document.getElementById("taskDescription").value;
+    const taskDate = document.getElementById("taskDate").value;
+    let dataTemp = new Date(taskDate);
+    dataTemp.setDate(dataTemp.getDate() + 1);
+    const taskPriorty = document.getElementById("taskPrio").value;
+    const taskCategoria = document.getElementById("taskCategoria").value;
+    const taskStatus = document.getElementById("statusTarefa");
+    const index = taskStatus.options.selectedIndex;
+    const taskStatusValue = taskStatus.options[index].innerHTML;
+
+    // Criar um novo objeto tarefa
+    return new Task(taskName, taskDescription, dataTemp.toLocaleString().substring(0, 10), taskPriorty, taskCategoria, taskStatusValue);
+}
