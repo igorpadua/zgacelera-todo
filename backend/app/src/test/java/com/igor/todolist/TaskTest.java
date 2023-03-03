07d8ed3211aa277aabf7098efd6b64f228cd2a67
igorpadua <igorpadua@tutanota.com>
2023-03-03 14:14:47 -0300

    feat: Atualizar dados

--- backend/app/src/test/java/com/igor/todolist/TaskTest.java
+++ backend/app/src/test/java/com/igor/todolist/TaskTest.java
@@ -25,4 +25,45 @@ public class TaskTest {
 
         Assertions.assertEquals(result, task);
     }
+    @Test
+    public void atualizarNome() {
+        Task task = new Task("Tarefa 1", "Descrição da tarefa 1", new Date(), (short) 1, "Categoria 1", "ToDo");
+        task.setNome("Tarefa 2");
+        Assertions.assertEquals("Tarefa 2", task.getNome());
+    }
+
+    @Test
+    public void atualizarDescricao() {
+        Task task = new Task("Tarefa 1", "Descrição da tarefa 1", new Date(), (short) 1, "Categoria 1", "ToDo");
+        task.setDescricao("Descrição da tarefa 2");
+        Assertions.assertEquals("Descrição da tarefa 2", task.getDescricao());
+    }
+
+    @Test
+    public void atualizarData() {
+        Task task = new Task("Tarefa 1", "Descrição da tarefa 1", new Date(), (short) 1, "Categoria 1", "ToDo");
+        task.setData_termino(new Date());
+        Assertions.assertEquals(new Date(), task.getData_termino());
+    }
+
+    @Test
+    public void atualizarPrioridade() {
+        Task task = new Task("Tarefa 1", "Descrição da tarefa 1", new Date(), (short) 1, "Categoria 1", "ToDo");
+        task.setNivel_prioridade((short) 2);
+        Assertions.assertEquals((short) 2, task.getNivel_prioridade());
+    }
+
+    @Test
+    public void atualizarCategoria() {
+        Task task = new Task("Tarefa 1", "Descrição da tarefa 1", new Date(), (short) 1, "Categoria 1", "ToDo");
+        task.setCategoria("Categoria 2");
+        Assertions.assertEquals("Categoria 2", task.getCategoria());
+    }
+
+    @Test
+    public void atualizarStatus() {
+        Task task = new Task("Tarefa 1", "Descrição da tarefa 1", new Date(), (short) 1, "Categoria 1", "ToDo");
+        task.setStatus("Doing");
+        Assertions.assertEquals("Doing", task.getStatus());
+    }
 }
